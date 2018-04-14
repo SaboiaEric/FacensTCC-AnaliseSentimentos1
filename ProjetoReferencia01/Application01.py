5abd1f7a7661efe56b0dc3b5a68939daf7cc5efd
Eric Saboia <saboia.eric@gmail.com>
2018-04-14 00:53:52 -0300

    Inicialize first project

---
+++ ProjetoReferencia01/Application01.py
@@ -0,0 +1,29 @@
+from textblob import TextBlob as tb
+import tweepy
+#Importado numpy para medir a popularidade dos modos
+#Não é necessário que isso seja feito :)
+import numpy as np 
+
+#Let's go to the code :)
+consumer_key = 'KJUcp9nAVbqjzrTgoMnX3huIy'
+consumer_secret = 'd9IB0sm8EXacPY6KcBxSJ2e3ULghvx4Y7zBRQQRKPaY4tS3OO7'
+
+access_token = '839943593322217473-7b2CeeFLaJFNMf7KUxMk6boKlVhemPK'
+access_token_secret = 'AOnWdLm8iKS1QunnsUmOq1zZCWAAff5YR49EzMTAPM6e4'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+#Variável que irá armazenar todos os Tweets com a palavra escolhida na função search da API
+public_tweets = api.search('World Cup 2018')
+#Variável que irá armazenar as polaridades
+analysis = None
+
+for tweet in public_tweets:
+    print(tweet.text)
+    analysis = tb(tweet.text)
+    print(analysis.sentiment.polarity)
+
+print('MÉDIA DE SENTIMENTO: ' + str(np.mean(analysis.sentiment.polarity)))
+
