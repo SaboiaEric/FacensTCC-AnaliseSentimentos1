d0138fb140fb7287b76ce7a321477b4bbe625603
Eric Saboia <saboia.eric@gmail.com>
2018-04-14 02:08:00 -0300

    Created database file example and improved the code

--- ProjetoReferencia01/Application01.py
+++ ProjetoReferencia01/Application01.py
@@ -5,6 +5,8 @@ import tweepy
 import numpy as np 
 
 #Let's go to the code :)
+
+#for item in tweets:
 consumer_key = 'KJUcp9nAVbqjzrTgoMnX3huIy'
 consumer_secret = 'd9IB0sm8EXacPY6KcBxSJ2e3ULghvx4Y7zBRQQRKPaY4tS3OO7'
 
@@ -16,14 +18,34 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 #Variável que irá armazenar todos os Tweets com a palavra escolhida na função search da API
-public_tweets = api.search('World Cup 2018')
+public_tweets = api.search('Eleicao2018')
 #Variável que irá armazenar as polaridades
 analysis = None
 
+#Declarando a lista contendo as informações dos tweets
+tweets = []
 for tweet in public_tweets:
-    print(tweet.text)
-    analysis = tb(tweet.text)
-    print(analysis.sentiment.polarity)
+    try:
+        #print(tweet.text)
+        analysis = tb(tweet.text)
+        #print(analysis.sentiment.polarity)
+        #print('MÉDIA DE SENTIMENTO: ' + str(np.mean(analysis.sentiment.polarity)))
+        tweets.append(tweet)
+    except:
+        continue
 
-print('MÉDIA DE SENTIMENTO: ' + str(np.mean(analysis.sentiment.polarity)))
+print(len(tweets))
+print("\n\n\n\n")
+#lista_completa = ''.join(tweets)
 
+#for item in tweets:
+    #print(str(item))
+    #print("\n")
+    #print(item.text +"\n")
+
+import datetime
+
+currentlyTime = str(datetime.datetime.now()).replace(':','_').replace(' ','_').replace('-','_')
+with open(currentlyTime + " - " + "miniDatabase.txt", "w") as output:
+    for tweet in tweets:
+        output.write(str(tweet.id)+ " -- " + tweet.text + "\n")
