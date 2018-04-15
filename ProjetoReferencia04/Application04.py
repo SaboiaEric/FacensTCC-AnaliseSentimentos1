8e3c192cb2a786844feb8ae9336d07b2f9c060e3
Eric Saboia <saboia.eric@gmail.com>
2018-04-15 16:44:11 -0300

    Finished first case

--- ProjetoReferencia04/Application04.py
+++ ProjetoReferencia04/Application04.py
@@ -3,6 +3,7 @@ from textblob import TextBlob as tb
 import tweepy
 import numpy as np 
 import datetime
+import re
 
 #Tweet structure
 def process_tweet(tweet):  
@@ -15,13 +16,33 @@ def process_tweet(tweet):
     d['user_name'] = tweet.user.name
     return d
 
+def remove_emoji(string):
+    emoji_pattern = re.compile("["
+                           u"\U0001F600-\U0001F64F"  # emoticons
+                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', string)
+
+#Creating safe tweet
+def create_string(item):
+    obj = "ID:" +  item['id_str'] + " - "
+    obj += "Criado em:" + str(item['created_at']) + " - " 
+    obj += "Texto:" + item['text'] + " - " 
+    obj += "Usuario ID:" + item['user_id_str'] + " - " 
+    obj += "Usuario Nome:"+ item['user_name'] + "\n"
+    return obj
+
 #Saving tweet
 def save_to_txt(tweets):
     currentlyTime = str(datetime.datetime.now()).replace(':','_').replace(' ','_').replace('-','_')
     with open(currentlyTime + "-" + "miniDatabase.txt", "w") as file:
         for item in tweets:
-            obj = item['id_str'] + "-" + str(item['created_at']) + "-" + item['text'] + "-" + item['user_id_str'] + "-" + item['user_name']
-            file.write(str(obj.encode('utf-8')))
+            tweet_safe = remove_emoji(create_string(item))
+            file.write(tweet_safe)
         
         #writer.writerow(list(tweets.values()))
 
@@ -44,8 +65,6 @@ for tweet in public_tweets:
         tweets.append(tw)
     except:
         print("ERROR TRY AGAIN\n")
-
-print("processou tudo")
 #print(list(tweets))
 save_to_txt(tweets)
 
