d5a879fded55fafdc810a1ac012eaf48da2f1eff
Eric Saboia <saboia.eric@gmail.com>
2018-10-04 23:14:10 -0300

    Adicionadas métricas para perfomance - Parte 1. Comentado corpus e algoritmos para desenvolvimento de métrica

--- SegundaFase/dados.py
+++ SegundaFase/dados.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from  processamento import processar
 
+'''
 tweets = pd.read_excel('DataSet_Bolsonaro.xlsx', sheet_name='DS-PNNeu-CL', encoding='utf-8')
 frases = tweets['Text'].str.lower()
 print("Arquivo: DataSet_Bolsonaro.xlsx	-  Folha: DS-PNNeu-CL")
@@ -17,7 +18,7 @@ tweets = pd.read_excel('DataSet_Bolsonaro.xlsx', sheet_name='DS-PNNeu-ID', encod
 frases = tweets['Text'].str.lower()
 print("Arquivo: DataSet_Bolsonaro.xlsx	-  Folha: DS-PNNeu-ID")
 processar(tweets, frases)
-
+'''
 tweets = pd.read_excel('DataSet_Bolsonaro.xlsx', sheet_name='DS-PN-ID', encoding='utf-8')
 frases = tweets['Text'].str.lower()
 print("Arquivo: DataSet_Bolsonaro.xlsx	-  Folha: DS-PN-ID")
--- SegundaFase/processamento.py
+++ SegundaFase/processamento.py
@@ -3,8 +3,10 @@ from sklearn.multiclass import OneVsRestClassifier, OneVsOneClassifier
 from sklearn.svm import LinearSVC
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.ensemble import AdaBoostClassifier
-from sklearn.metrics import average_precision_score ,accuracy_score ,confusion_matrix, classification_report
+from sklearn.metrics import average_precision_score, accuracy_score, confusion_matrix, classification_report, f1_score, precision_score, recall_score
 from collections import Counter
+import matplotlib.pyplot as plt
+import itertools
 import pandas as pd
 import numpy as np
 import nltk
@@ -34,6 +36,9 @@ def teste_real(modelo, validacao_dados, validacao_marcacoes):
     resultado = modelo.predict(validacao_dados)
     metricas(resultado,validacao_marcacoes)
 
+def revalidacao_treino(modelo, treino_dados, treino_marcacoes):
+    resultado = modelo.predict(treino_dados)
+
 def metricas(resultado,validacao_marcacoes):
     #Apresentação do report contendo precision, recall and F-measures
     if len(set(resultado)) > 2:
@@ -41,18 +46,61 @@ def metricas(resultado,validacao_marcacoes):
         #os valores acima estão na ordem correta
         print(classification_report(validacao_marcacoes, resultado, target_names=target_names))
     else:
+        print("Classification Report\n")
         target_names = ['negativo', 'positivo']
         #os valores acima estão na ordem correta
-        print(classification_report(validacao_marcacoes, resultado, target_names=target_names))
-        
+        print(classification_report(validacao_marcacoes, resultado, target_names=target_names))       
+
         #Apresentação dos true positive, false positive, true negative e false negative
         tn, fp, fn, tp = confusion_matrix(validacao_marcacoes, resultado).ravel() 
-        msg = "true positive: {0} - false positive: {1} - true negative: {2} - false negative: {3}".format(str(tn),str(fp),str(fn),str(tp))
-        print(msg)
+        msg = "True Positive: {0} \nFalse Positive: {1}\nTrue Negative: {2}\nFalse Negative: {3}".format(str(tn),str(fp),str(fn),str(tp))
+        print(msg+"\n")
+
+        matrix_confusao(validacao_marcacoes, resultado,target_names)
+
+        print("Metrics\n")
+        print("F-score: " + str(f1_score(validacao_marcacoes, resultado)))
+        print("Precision: " + str(precision_score(validacao_marcacoes, resultado))) 
+        print("Recall: " + str(recall_score(validacao_marcacoes, resultado))) 
+        #Cria estatistica dos resultados.
+        taxa_de_acerto_base = max(Counter(validacao_marcacoes).values()) * 100 / len(validacao_marcacoes)
+        print("Taxa de acerto base: %f" % taxa_de_acerto_base)
 
     #Apresentação da accuracy e resultado final
-    msg = "ETAPA VALIDAÇÃO - Taxa de acerto com dados do mundo real: " + str(accuracy_score(validacao_marcacoes, resultado))
-    print(msg)
+    print("Acurácia: " + str(accuracy_score(validacao_marcacoes, resultado)))
+
+def plot_confusion_matrix(cm, classes,normalize=False,title='Confusion matrix',cmap=plt.cm.Blues):
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print('Matrix de Confusão Normalizada')
+    else:
+        print('Matrix de Confusão Não Normalizada')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+
+def matrix_confusao(validacao_marcacoes, resultado, target_names):
+    cnf_matrix = confusion_matrix(validacao_marcacoes, resultado)
+    np.set_printoptions(precision=2)
+    plt.figure()
+    plot_confusion_matrix(cnf_matrix, classes=target_names,title='Matrix de Confusão, sem normalização')
 
 def processar(tweets, frases):
     #Descomente caso seja primeira vez de execução. Funcionalidaeds de processamento
@@ -105,7 +153,7 @@ def processar(tweets, frases):
     
     
     resultados = {}
-    
+    '''
     # OneVsRestClassifier - LinearSVC
     modeloOneVsRest = OneVsRestClassifier(LinearSVC(random_state=0))
     resultadoOneVsRest = fit_and_predict("OneVsRest", modeloOneVsRest, treino_dados, treino_marcacoes)
@@ -115,7 +163,7 @@ def processar(tweets, frases):
     modeloOneVsOne = OneVsOneClassifier(LinearSVC(random_state=0))
     resultadoOneVsOne = fit_and_predict("OneVsOne", modeloOneVsOne, treino_dados, treino_marcacoes)
     resultados[resultadoOneVsOne] = modeloOneVsOne
-    
+    '''
     # MultinomialNB
     modeloMultinomial = MultinomialNB(alpha=1.0, fit_prior=True)
     resultadoMultinomial = fit_and_predict("MultinomialNB", modeloMultinomial, treino_dados, treino_marcacoes)
@@ -130,17 +178,10 @@ def processar(tweets, frases):
     maximo = max(resultados)
     vencedor = resultados[maximo]
     
-    print("Vencedor: " + str(vencedor))
+    print("Vencedor: " + str(vencedor)+"\n")
     
     #Aplica o modelo vencedor em um caso do mundo real, utilizando dados nunca vistos
     vencedor.fit(treino_dados, treino_marcacoes)
+    revalidacao_treino(vencedor, treino_dados, treino_marcacoes)
     teste_real(vencedor, validacao_dados, validacao_marcacoes)
-    
-    #Cria estatistica dos resultados.
-    acerto_base = max(Counter(validacao_marcacoes).values())
-    taxa_de_acerto_base = 100.0 * acerto_base / len(validacao_marcacoes)
-    print("Taxa de acerto base: %f" % taxa_de_acerto_base)
-    
-    total_de_elementos = len(validacao_dados)
-    print("Total de teste: %d" % total_de_elementos)
 
