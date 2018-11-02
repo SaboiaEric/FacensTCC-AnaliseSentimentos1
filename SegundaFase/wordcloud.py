a66459bd4e4fb2a2e4d8e6f37b5d02f6424353d0
Saboia <Eric.Saboia@br.experian.com>
2018-11-02 00:57:43 -0300

    Adicionado o código para auxiliar na wordcloud

--- SegundaFase/wordcloud.py
+++ SegundaFase/wordcloud.py
@@ -4,6 +4,7 @@ import nltk
 import json
 import pickle
 import csv
+from collections import Counter
 
 tweets = pd.read_excel('DataSet_Bolsonaro.xlsx', sheet_name='DS-PN-ID', encoding='utf-8')
 frases = tweets['Text'].str.lower()
@@ -11,31 +12,51 @@ frases = tweets['Text'].str.lower()
 print("Arquivo: DataSet_Bolsonaro.xlsx	-  Folha: DS-PN-ID")
 
 textosQuebrados = [nltk.tokenize.word_tokenize(frase) for frase in frases]
-
-#aplica as stop words, isto é, remove os pronomes e etc.
 stopwords = nltk.corpus.stopwords.words('portuguese')
+stemmer = nltk.stem.RSLPStemmer();
+'''    
+dicionario = set()
+for lista in textosQuebrados:
+    validas = [stemmer.stem(palavra) for palavra in lista if palavra not in stopwords and len(palavra) > 2]
+    dicionario.update(validas)
+    
+
+totalDePalavras = len(dicionario)
+tuplas = zip(dicionario, range(totalDePalavras))
+'''
+palavras = []
+for lista in textosQuebrados:
+    for palavra in lista:
+        if palavra not in stopwords and len(palavra) > 2:
+    	    palavras.append(palavra)    
+
+
+#print(palavras)
+for elemento in palavras:
+	print(elemento +": "+ str(palavras.count(elemento)))
+	#print(palavras[elemento] +": "+ (Counter(palavras[elemento]).values()))
+
 
 '''
-lista_palavras = [] 
-for lista in textosQuebrados:
-    	lista_palavras.append(lista)
-'''
-#aplica steem, isto é, deixa a palavra na forma raiz
-stemmer = nltk.stem.RSLPStemmer();
-    
-    
 dicionario = []
 for lista in textosQuebrados:
     for palavra in lista: 
-    	if palavra not in stopwords and len(palavra) > 2:
-    		dicionario.append(palavra)
+        if palavra not in stopwords and len(palavra) > 2:
+            tuplas = zip(dicionario, range(totalDePalavras))
 
-'''totalDePalavras = len(dicionario)
+
+dicionario = []
+for lista in textosQuebrados:
+    print(lista)
+
+totalDePalavras = len(dicionario)
 
 print(tuplas)
-dicionario = sorted(dicionario)'''
+dicionario = sorted(dicionario)
 
 print(dicionario.count("bolsonaro"))
 with open('your_file.txt', 'w', encoding='utf-8') as f:
     for item in dicionario:
         f.write(str(dicionario.count(item)) +" - " + item +"\n")
+
+'''
